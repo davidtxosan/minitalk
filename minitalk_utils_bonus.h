cd901747014171afb7122ea593c00193fc3749ee
galdor30 <dsjaramillo@hotmail.com>
2022-07-20 19:51:09 +0200

    fixing

--- minitalk_utils_bonus.h
+++ minitalk_utils_bonus.h
@@ -6,18 +6,18 @@
 /*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 15:14:18 by usuario           #+#    #+#             */
-/*   Updated: 2022/07/20 19:41:31 by usuario          ###   ########.fr       */
+/*   Updated: 2022/07/20 19:44:39 by usuario          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_UTILS_BONUS_H
 # define MINITALK_UTILS_BONUS_H
 
-# include <sys/types.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/types.h>
 
 void	ft_error(const char *str, int flag);
 int		ft_atoi(const char *str);
